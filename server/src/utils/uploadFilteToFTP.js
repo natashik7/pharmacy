477b89b79954f8983013dd0a096e972340cff8f8
zhalgasmendykulov <likeit201539@gmail.com>
2024-12-11 14:28:14 +0300

    fix FTP upload file

--- server/src/utils/uploadFilteToFTP.js
+++ server/src/utils/uploadFilteToFTP.js
@@ -1,17 +1,29 @@
-const Client = require('basic-ftp');
+const { Client } = require('basic-ftp');
+const { Readable } = require('stream'); // Импортируем Readable из stream
 
 const uploadFileToFTP = async (file) => {
   const client = new Client();
   try {
+    client.ftp.verbose = true; // Включите подробное логирование
+
     await client.access({
-      host: 'your_ftp_host',
-      user: 'your_ftp_user',
-      password: 'your_ftp_password',
+      host: '178.20.42.235',
+      user: 'elf',
+      password: 'SMelfVSNiLnTD',
       secure: false,
     });
-    await client.uploadFrom(file.buffer, file.originalname);
+
+    const serverPath = `/obmen/${file.originalname}`;
+
+    // Преобразуем Buffer в поток
+    const stream = new Readable();
+    stream.push(file.buffer);
+    stream.push(null); // Завершаем поток
+
+    await client.uploadFrom(stream, serverPath); // Используем поток для загрузки
   } catch (error) {
-    console.log(error);
+    console.error('Error uploading file to FTP:', error);
+    throw error;
   } finally {
     client.close();
   }
